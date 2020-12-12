83195db992fc015a2ac3ed42a382e88c8cdca5d2
Promiselxg <okeydeede@gmail.com>
2020-12-12 11:49:14 -0800

    Finished Product Display Screen

--- frontend/src/components/Rating.js
+++ frontend/src/components/Rating.js
@@ -63,7 +63,7 @@ const Rating = ({ value, text, color }) => {
           }
         ></i>
       </span>
-      <span>{text && text}</span> reviews
+      <span>{text && text}</span>
     </div>
   )
 }
