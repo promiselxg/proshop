f92cb48d1a4faa7696ea17aa7ac3c11e66d8486e
Promiselxg <okeydeede@gmail.com>
2020-12-18 16:33:54 +0100

    added protect Middleware to protect private routes

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,6 @@
 import AsyncHandler from 'express-async-handler'
 import User from '../models/userModel.js'
+import generateToken from '../utils/generateToken.js'
 
 //  @desc   Authenticate User & get Token
 //  @route  POST api/users/login
@@ -10,14 +11,14 @@ const authUser = AsyncHandler(async (req, res) => {
 
   const user = await User.findOne({ email })
 
-  //    check if user exist
+  //    check if user exist and match password
   if (user && (await user.matchPassword(password))) {
     res.json({
       _id: user._id,
       name: user.name,
       email: user.email,
       isAdmin: user.isAdmin,
-      token: null,
+      token: generateToken(user._id),
     })
   } else {
     res.status(401)
@@ -25,4 +26,22 @@ const authUser = AsyncHandler(async (req, res) => {
   }
 })
 
-export { authUser }
+//  @desc  Get User Profile
+//  @route  GET api/users/profile
+//  @access Private
+const getUserProfile = AsyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id)
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    })
+  } else {
+    res.status(404)
+    throw new Error('User not Found')
+  }
+})
+
+export { authUser, getUserProfile }
---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,34 @@
+import jwt from 'jsonwebtoken'
+import User from '../models/userModel.js'
+import AsyncHandler from 'express-async-handler'
+
+// protect routes from NOT logged in users
+const protect = AsyncHandler(async (req, res, next) => {
+  let token
+
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    try {
+      token = req.headers.authorization.split(' ')[1]
+      const decoded = jwt.verify(token, process.env.JWT_SECRET)
+
+      // return user data from DB except the password ('-password')
+      req.user = await User.findById(decoded.id).select('-password')
+
+      next()
+    } catch (error) {
+      console.error(error)
+      res.status(401)
+      throw new Error('Not Authorized, Token Failed')
+    }
+  }
+
+  if (!token) {
+    res.status(401)
+    throw new Error('Not Authorized, No Token')
+  }
+})
+
+export { protect }
