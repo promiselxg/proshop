a6489444e7aaf8d23b284b57bdb809667eb4189a
Promiselxg <okeydeede@gmail.com>
2020-12-18 14:41:35 +0100

    added user authentication

---
+++ backend/controllers/userController.js
@@ -0,0 +1,28 @@
+import AsyncHandler from 'express-async-handler'
+import User from '../models/userModel.js'
+
+//  @desc   Authenticate User & get Token
+//  @route  POST api/users/login
+//  @access Public
+const authUser = AsyncHandler(async (req, res) => {
+  // get data from form body
+  const { email, password } = req.body
+
+  const user = await User.findOne({ email })
+
+  //    check if user exist
+  if (user && (await user.matchPassword(password))) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: null,
+    })
+  } else {
+    res.status(401)
+    throw new Error('Invalid Email or Password')
+  }
+})
+
+export { authUser }
--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,8 @@ import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import connectDB from './config/db.js'
 
 import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js'
+
 dotenv.config()
 
 // Connect to Database
@@ -13,12 +15,15 @@ connectDB()
 //  Initialize Express Server
 const app = express()
 
+app.use(express.json())
+
 app.get('/', (req, res) => {
   res.send('API is Running...')
 })
 
 //  Moute Routes
 app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
 
 //  Error Handler - File not Found
 app.use(notFound)
