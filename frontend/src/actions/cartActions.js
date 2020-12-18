7a221c522eb4e01bdd2ac0f5aef98742e83d5a0d
Promiselxg <okeydeede@gmail.com>
2020-12-18 13:17:19 +0100

    added remove item from cart functionality

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
-import { CART_ADD_ITEM } from '../constants/cartConstants'
+import { CART_ADD_ITEM, CART_REMOVE_ITEM } from '../constants/cartConstants'
 
+// add item to cart
 export const addToCart = (id, qty) => async (dispatch, getState) => {
   const { data } = await axios.get(`/api/products/${id}`)
 
@@ -17,5 +18,15 @@ export const addToCart = (id, qty) => async (dispatch, getState) => {
   })
 
   //save to local storage
+  localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems))
+}
+
+// remove item from cart
+export const removeFromCart = (id) => (dispatch, getState) => {
+  dispatch({
+    type: CART_REMOVE_ITEM,
+    payload: id,
+  })
+
   localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems))
 }
--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -3,11 +3,13 @@ import { Link } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import { Row, Col, ListGroup, Image, Form, Button, Card } from 'react-bootstrap'
 import Message from '../components/Message'
-import { addToCart } from '../actions/cartActions'
+import { addToCart, removeFromCart } from '../actions/cartActions'
 
 const CartScreen = ({ match, location, history }) => {
   const productId = match.params.id
 
+  // get the product quantity, if it does not exist, default it to 1
+  // /api/products/:id?qty=1
   const qty = location.search ? Number(location.search.split('=')[1]) : 1
 
   const dispatch = useDispatch()
@@ -22,7 +24,11 @@ const CartScreen = ({ match, location, history }) => {
     }
   }, [dispatch, productId, qty])
 
-  const removeFromCartHandler = (id) => {}
+  // remove from cart function
+  const removeFromCartHandler = (id) => {
+    dispatch(removeFromCart(id))
+  }
+  // check out function
   const checkOutHandler = () => {
     history.push('/login?redirect=shipping')
   }
