116f79c07466b144117abab6ec60614cb51f240e
Promiselxg <okeydeede@gmail.com>
2020-12-24 09:02:59 +0100

    added order summary

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -36,4 +36,48 @@ const addOrderItems = AsyncHandler(async (req, res) => {
   }
 })
 
-export { addOrderItems }
+//  @desc   GET Order by ID
+//  @route  GET api/orders/:id
+//  @access Private
+const getOrderById = AsyncHandler(async (req, res) => {
+  // .populate is used to get addidtional information associated to a particular DB collection
+  // populate by User TAble, get name and email
+  const order = await Order.findById(req.params.id).populate(
+    'user',
+    'name email'
+  )
+
+  if (order) {
+    res.json(order)
+  } else {
+    res.status(404)
+    throw new Error('Order not Found')
+  }
+})
+
+//  @desc   Update order to Paid
+//  @route  GET api/orders/:id/pay
+//  @access Private
+const updateOrderToPAid = AsyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id)
+
+  if (order) {
+    order.isPaid = true
+    order.paidAt = Date.now()
+    // gotten from PayPal
+    order.paymentResult = {
+      id: req.body.id,
+      status: req.body.status,
+      update_time: req.body.status,
+      email_address: req.body.payer.email_address,
+    }
+
+    const updatedOrder = await order.save()
+    res.json(updatedOrder)
+  } else {
+    res.status(404)
+    throw new Error('Order not Found')
+  }
+})
+
+export { addOrderItems, getOrderById, updateOrderToPAid }
